1161060c0c5e69e9dfb5f67a4c41800901aaec23
Fahmidur-arnob <arnobdev93@gmail.com>
2022-11-08 01:07:15 +0600

    Done with the banner section but responsive is still not done yet

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,68 +1,47 @@
 import React from 'react';
 import imgOne from '../../../assets/images/carouOne.avif';
 import imgTwo from '../../../assets/images/carouTwo.avif';
-import imgThree from '../../../assets/images/carouThree.avif';
 import imgFour from '../../../assets/images/carouFour.avif';
 import imgFive from '../../../assets/images/caourFive.avif';
-import imgSix from '../../../assets/images/carouSix.avif';
-import './Banner.css';
+import BannerItem from './BannerItem';
+
+const bannerData = [
+    {
+        image: imgOne,
+        prev: 4,
+        id: 1,
+        next: 2
+    },
+    {
+        image: imgTwo,
+        prev: 1,
+        id: 2,
+        next: 3
+    },
+    {
+        image: imgFour,
+        prev: 2,
+        id: 3,
+        next: 4
+    },
+    {
+        image: imgFive,
+        prev: 3,
+        id: 4,
+        next: 1
+    }
+]
 
 const Banner = () => {
     return (
         <div>
-            <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <div className='caro-img'>
-                        <img src={imgOne} className="w-full" />
-                    </div>
-                    <div className="absolute flex justify-end gap-3 transform -translate-y-1/2 left-10 top-1/4">
-                        <h1 className='text-6xl text-neutral-300'>
-                            I freeze <br /> <span className='text-rose-400 font-semibold'>MOMENTS</span><br />
-                            and make them <br /> <span className='text-rose-400 font-semibold'>ALIVE</span>
-                        </h1>
-                    </div>
-
-                    <div className="absolute flex justify-end gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src={imgTwo} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src={imgThree} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src={imgFour} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src={imgFive} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src={imgSix} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
+            <div className="carousel w-full py-5">
+                {
+                    bannerData.map(slide => <BannerItem
+                    key={slide.id}
+                    slide={slide}
+                    ></BannerItem>)
+                }
             </div>
         </div>
     );
---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './BannerItem.css';
+
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-img'>
+                <img src={image} alt="" className="carou-img rounded-xl" />
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                <h1 className='text-6xl font-bold text-gray-50'>
+                    I freeze <br />
+                    <span className='font-bold text-rose-400'>MOMENTS</span><br />
+                    and make them <br />
+                    <span className='font-bold text-rose-400'>ALIVE</span>
+                </h1>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
