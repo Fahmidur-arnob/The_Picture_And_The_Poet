b0a6fffe9a4af4e165eb434d374b266415949267
Fahmidur-arnob <arnobdev93@gmail.com>
2022-11-09 00:35:13 +0600

    Almost done with the UI part

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 import {FaUser} from 'react-icons/fa';
+import './Header.css';
 
 const Header = ({ props }) => {
     const { user, logOut } = useContext(AuthContext);
@@ -14,14 +15,14 @@ const Header = ({ props }) => {
     }
     return (
         <div>
-            <div className="navbar bg-rose-600 text-gray-50">
+            <div className="navbar bg-blue-900 text-gray-50">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-rose-500 rounded-box w-52">
-                            <li><Link>Home</Link></li>
+                            <li><Link to='/'>Home</Link></li>
                             <li tabIndex={0}>
                                 <Link className="justify-between" to='/services'>
                                     Services
@@ -32,15 +33,15 @@ const Header = ({ props }) => {
                     </div>
                     <Link className="normal-case text-xl font-bold">Picture & Poetry</Link>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
-                        <li><Link>Home</Link></li>
+                <div className="navbar-center hidden  lg:flex">
+                    <ul className="menu menu-horizontal p-0 navUL">
+                        <li><Link className='font-semibold border border-gray-50 hover:bg-indigo-700' to='/'>Home</Link></li>
                         <li tabIndex={0}>
-                            <Link to='/services'>
+                            <Link to='/services' className='font-semibold border border-gray-50 hover:bg-indigo-700 hover:text-gray-50'>
                                 Services
                             </Link>
                         </li>
-                        <li><Link to='/appointment'>Appointment</Link></li>
+                        <li><Link to='/appointment' className='font-semibold border border-gray-50 hover:bg-indigo-700'>Appointment</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end gap-2 mx-4">
@@ -48,12 +49,17 @@ const Header = ({ props }) => {
                         user?.uid ?
                             <>
                                 <span className=''>{user?.displayName}</span>
-                                <button onClick={handleLogOut}>LogOut</button>
+                                <Link className="btn bg-blue-900 hover:bg-indigo-900 border border-gray-50" to='/register'>Log Out</Link>
                             </>
                             :
                             <>
-                                <Link className="btn" to='/login'>Login</Link>
-                                <Link className="btn" to='/register'>Register</Link>
+                                <Link className="btn bg-blue-900 border border-gray-50
+                                hover:border-gray-50
+                                hover:bg-indigo-700" to='/login'>Login</Link>
+
+                                <Link className="btn bg-blue-900 border border-gray-50
+                                hover:border-gray-50
+                                hover:bg-indigo-700" to='/register'>Register</Link>
                             </>
                     }
                 </div>
